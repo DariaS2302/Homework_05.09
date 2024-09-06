f253deff7b8f2fb6fd1c58f3c953bba27c83063f
Sarycheva Daria <d.saricheva@gmail.com>
2024-09-06 16:22:55 +0300

    added .DemogaFormTest

--- src/test/java/DemoqaFormTest.java
+++ src/test/java/DemoqaFormTest.java
@@ -2,23 +2,19 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class demoqaFormTest {
-
-    private File media;
+public class DemoqaFormTest {
 
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
     }
 
 
